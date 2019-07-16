45b64fd2222b1b9e0fc6b9f85885d7686bfd68b5
faluis <florencio_luis@gesterno.es>
2019-07-16 14:43:55 +0200

    Curso_de_introduccion_a_la_programacion_201907161443000

---
+++ ordenlista.py
@@ -0,0 +1,24 @@
+lista = [8.17,90,1,5,44,1.32]
+
+lista.sort(reverse = True)
+
+mayor = lista[0]
+menor = lista[5]
+
+print(lista)
+print(mayor)
+print(menor)
+
+menor = min(lista)
+mayor = max(lista)
+print(menor)
+print(mayor)
+
+longitud = len(lista)
+print(longitud)
+
+resultado = 8 in lista
+print(resultado)
+
+indice = lista.index(8.17)
+print(indice)
