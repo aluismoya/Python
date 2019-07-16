b36e99bb5f8af240f5f0f22c1bfcfa38cabf8cce
faluis <florencio_luis@gesterno.es>
2019-07-16 11:04:38 +0200

    Curso_de_introduccion_a_la_programacion_201907161119000

---
+++ aritmetico.py
@@ -0,0 +1,13 @@
+print("Ingresa el primer valor:")
+num1 = int (input())
+print("Ingresa el segundo valor:")
+num2 = int (input())
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print("la suma de num1 y num2 es igual a:", suma)
+print("la resta de num1 y num2 es igual a:", resta)
+print("la multiplicacion de num1 y num2 es igual a:", multiplicacion)
+print("la division de num1 y num2 es igual a:", division)
