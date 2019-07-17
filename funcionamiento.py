43d19ec7b62c26dd649e6494487183e1ba045f1d
faluis <florencio_luis@gesterno.es>
2019-07-17 15:08:09 +0200

    Curso_profesional_de_python_20190717150700

---
+++ funcionamiento.py
@@ -0,0 +1,17 @@
+
+diccionario = {}
+
+diccionario["nombre"] = "Codi"
+
+valor =diccionario["nombre"]
+
+diccionario["nombre"] = 90
+
+print(diccionario)
+
+print (valor)
+
+diccionario2 = {"a":1, "b":2, "c":3, "a":4}
+
+print(diccionario2)
+
---
+++ llaves.py
@@ -0,0 +1,14 @@
+diccionario = {"a":1, "b":2, "c":3, "a":4, "e":5, "f":6, "g":7, "h":8}
+
+resultado1 = diccionario.keys()
+resultado1 = tuple(resultado1)
+
+resultado2 = diccionario.values()
+resultado2 = tuple(resultado2)
+
+resultado3 = diccionario.items()
+resultado3 = tuple(resultado3)
+
+print(resultado1)
+print(resultado2)
+print(resultado3)
